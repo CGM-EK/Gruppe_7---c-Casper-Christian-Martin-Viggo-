1ab5111e3a1fedc2f5de41482f0d26fc4f20d66b
CGM-EK <cph-cm426@stud.ek.dk>
2025-11-02 19:55:34 +0100

    Add files via upload

--- Ola 3 Opgave 2.R
+++ Ola 3 Opgave 2.R
@@ -36,10 +36,8 @@ ggplot(data = w.indicators1, aes(x = spg[1:12], y = w.indicators1[,1]))+
 # pcr.fit på spg for den optimale indikator fra opg. 1
 pcr.fit.opt <- pcr(f.tillidsammen$pfv ~
                      forbrugertillid$Spg3+
-                     forbrugertillid$Spg6+
                      forbrugertillid$Spg8+
                      forbrugertillid$Spg9+
-                     forbrugertillid$Spg10+
                      forbrugertillid$Spg11+
                      forbrugertillid$Spg12,
                    validation = "CV", scale = T)
@@ -49,24 +47,22 @@ loadings.pcr.fit.opt <- pcr.fit.opt$loadings
 
 validationplot(pcr.fit.opt, val.type = "MSEP")
 
-w.indicators1opt <- as.data.frame(loadings.pcr.fit.opt[1:7, 2]^2)
+w.indicators1opt <- as.data.frame(loadings.pcr.fit.opt[1:5, 2]^2)
 sum(w.indicators1opt)
 
-w.indicators1opt$spg <- c("Spg3", "Spg6", "Spg8", "Spg9", "Spg1", "Spg11", "Spg12")
+w.indicators1opt$spg <- c("Spg3", "Spg8", "Spg9", "Spg11", "Spg12")
 w.indicators1opt$spg <- factor(w.indicators1opt$spg,
                                levels = paste0("Spg", 1:12))
 
-ggplot(data = w.indicators1opt, aes(x = spg[1:7], y = w.indicators1opt[,1]))+
+ggplot(data = w.indicators1opt, aes(x = spg[1:5], y = w.indicators1opt[,1]))+
   geom_bar(fill = "darkolivegreen4", stat = "identity")+
   ylab("Vægt")+xlab("Spørgsmål")+ labs(title = "Vægtning af spørgsmål på den optimale indikator")+
   theme_minimal()
 
 #pfv
 optimalvektor <- as.data.frame(forbrugertillid$Spg3*as.numeric(w.indicators1[3,1])+
-                                 forbrugertillid$Spg6*as.numeric(w.indicators1[6,1])+
                                  forbrugertillid$Spg8*as.numeric(w.indicators1[8,1])+
                                  forbrugertillid$Spg9*as.numeric(w.indicators1[9,1])+
-                                 forbrugertillid$Spg10*as.numeric(w.indicators1[10,1])+
                                  forbrugertillid$Spg11*as.numeric(w.indicators1[11,1])+
                                  forbrugertillid$Spg12*as.numeric(w.indicators1[12,1]))
 
@@ -75,10 +71,10 @@ summary(lm.opt.vægt)
 
 k3opt <- optimalvektor[99,1]
 #definerer koefficienter for den lm
-stdopt <- 0.72555
-estimateopt <- -0.87717
+stdopt <- 1.2646
+estimateopt <- -8.4992
 
-#forudsigelse for k4
+#forudsigelse for k3
 pfvforudsigelsek3opt <- estimateopt+stdopt*k3opt
 
 # Opgave 2.4
